c0ee547aa7615be89cb2fae3a901efc48d1ab3a2
udream-Wangzh <wangzh@udream.cn>
2021-09-18 18:13:26 +0800

    【fix】添加滑动插件

--- build/rollup.config.dev.js
+++ build/rollup.config.dev.js
@@ -2,6 +2,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 // import postcss from 'rollup-plugin-postcss'
 import scss from 'rollup-plugin-scss'
+import css from "rollup-plugin-import-css";
 
 export default {
   input: 'src/index.js',
@@ -18,6 +19,7 @@ export default {
     //   extract: true,
     //   extract: 'image-preview.css'
     // }),
+    css(),
     scss({
       output: 'dist/image-preview.css'
     }),
--- src/code/createSwiper.js
+++ src/code/createSwiper.js
@@ -1,15 +1,18 @@
+import Touches from './swiper'
+
 function createSwiper(options) {
-    const html = createHtml(options)
-    document.body.appendChild(html)
+    const html = createHtml(options);
+    document.body.appendChild(html);
+
+    const slider = new Touches('Image-preveiw');
 }
 
 function createHtml(options) {
-    const wrap = document.createElement('div')
+    const wrap = document.createElement('div');
     wrap.className = 'image-preview-wrap'
+    wrap.id = 'Image-preveiw'
 
-    let str = `
-        <div>121212</div>
-    `
+    // let str = ``
     wrap.innerHTML = str
     
     return wrap;
