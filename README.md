9c9d95a2b5893bc699dad9bec099301b45179ab0
udream-Wangzh <wangzh@udream.cn>
2021-11-17 15:33:01 +0800

    【dosc】编写使用文档以及案例访问

--- README.md
+++ README.md
@@ -1,7 +1,37 @@
 # 移动端图片预览
 
-### 
+![1](https://github.com/WangZhenHao/image-preview/blob/master/example/image/1.jpg)
+![2](https://github.com/WangZhenHao/image-preview/blob/master/example/image/2.jpg)
+![3](https://github.com/WangZhenHao/image-preview/blob/master/example/image/3.jpg)
+
+```
+引入：
+     dist/image-preview.css  
+     dist/image-preview.js
 
 
+显示：
+imagePrevew.open({
+    list: [
+      'https://wx-test.51yxm.com/html5/root/MiniPragram/udream/images/test.jpg',
+      'https://img01.yzcdn.cn/vant/apple-2.jpg',
+      'http://img1.baidu.com/it/u=1027312569,4018762616&fm=26&fmt=auto&gp=0.jpg',
+    ],
+    index: 0
+})
+
+关闭：
+imagePrevew.close()
+```
+
+```
+imagePrevew.open({}) 参数说明
+
+list                   array               图片列表（默认： []）            如：['xxx', 'xxx']
+index                  number              显示第几张图片（默认：0）         如： 1
+```
+
+### 手机预览：
+![4](https://github.com/WangZhenHao/image-preview/blob/master/example/image/4.jpg)
 
 ### QQ交流群：475870039 
--- dist/image-preview.js
+++ dist/image-preview.js
@@ -21,8 +21,6 @@
       //动画执行的时间;
       slider_dis: config.slider_dis || 50,
       //滑动灵敏度;
-      loop: config.loop || false,
-      //是否开启无限轮播;
       images: config.list || []
     };
     this.index = config.index || 0;
@@ -33,6 +31,8 @@
     this.init();
   }
 
+  var verticalClass = 'swiper-wrap-preview__image--vertical';
+  var hideClass = 'swiper-wrap-preview__hide';
   Touches.prototype = {
     init: function init() {
       if (this.set.images.length === 1) {
@@ -57,11 +57,37 @@
       var str = '';
 
       for (var i = 0; i < images.length; i++) {
-        str += "\n        <div class=\"swiper-wrap-item\">\n            <div class=\"swiper-wrap-preview__image\">\n                <img class=\"swiper-wrap__image\" src=\"".concat(images[i], "\" />\n            </div>\n        </div>\n      ");
+        str += "\n        <div class=\"swiper-wrap-item\">\n            <div class=\"swiper-wrap-preview__image ".concat(hideClass, "\">\n                <img class=\"swiper-wrap__image\" src=\"").concat(images[i], "\" />\n                <div class=\"loading swiper-wrap__image--loading\"></div>\n            </div>\n        </div>\n      ");
       }
 
       div.innerHTML = str;
       this.container.appendChild(div);
+      this.imageLoad();
+    },
+    imageLoad: function imageLoad() {
+      var imageList = this.container.getElementsByClassName('swiper-wrap__image'); // console.log(imageList instanceof Array)
+
+      for (var i = 0; i < imageList.length; i++) {
+        imageList[i].onload = function (event) {
+          var _event$target = event.target,
+              naturalWidth = _event$target.naturalWidth,
+              naturalHeight = _event$target.naturalHeight;
+          var parentNode = event.target.parentNode;
+          var vertical = naturalHeight / naturalWidth > window.innerHeight / window.innerWidth;
+
+          if (vertical) {
+            parentNode.classList.add(verticalClass);
+          }
+
+          parentNode.classList.remove(hideClass);
+          parentNode.removeChild(event.target.nextElementSibling);
+        };
+      } // imageList.forEach(item => {
+      //   item.onLoad = function(event) {
+      //     console.log(event)
+      //   }
+      // })
+
     },
 
     /**
@@ -164,6 +190,13 @@
       this.swiperWrap.addEventListener('touchmove', this.Tmove);
       this.swiperWrap.addEventListener('touchend', this.Tend);
     },
+    destrory: function destrory() {
+      this.swiperWrap.removeEventListener('touchstart', this.Tstart);
+      this.swiperWrap.removeEventListener('touchmove', this.Tmove);
+      this.swiperWrap.removeEventListener('touchend', this.Tend);
+      var imagePreveiw = document.querySelector('#imagePreveiw');
+      imagePreveiw.parentNode.removeChild(imagePreveiw);
+    },
 
     /**
      * 清除自动轮播计时器
@@ -225,6 +258,8 @@
     return Math.hypot(stop.x - start.x, stop.y - start.y);
   }
 
+  var MAXSCALENUM = 5;
+
   function scaleImage(swiper) {
     this.swiper = swiper;
     this.store = {
@@ -273,6 +308,8 @@
 
         _this.store.originScale = _this.store.scale || 1;
 
+        _this.imgageSacaleAnimate(false);
+
         _this.imageScale();
       };
 
@@ -312,13 +349,12 @@
           });
           var newScale = _this.store.originScale * zoom;
 
-          if (newScale > 3) {
-            newScale = 3;
-          }
+          if (newScale > MAXSCALENUM) {
+            newScale = MAXSCALENUM;
+          } // if(newScale < 1) {
+          //     newScale = 1;
+          // }
 
-          if (newScale < 1) {
-            newScale = 1;
-          }
 
           _this.store.scale = newScale;
 
@@ -330,30 +366,52 @@
           var deltaY = events.clientY - _this.move.startY;
           var moveX = deltaX + _this.move.startMoveX;
           var moveY = deltaY + _this.move.startMoveY;
-
-          var distanceX = _this.maxMoveX();
-
-          var distanceY = _this.maxMoveY(); // console.log(this.store.scale, moveX, distance, '----------------->', range(moveX, -distance, distance))
+          _this.distanceX = _this.maxMoveX();
+          _this.distanceY = _this.maxMoveY(); // console.log(this.store.scale, moveX, distance, '----------------->', range(moveX, -distance, distance))
           // console.log(distanceY)
 
+          _this.move.moveX = range(moveX, -_this.distanceX, _this.distanceX);
+          _this.move.moveY = range(moveY, -_this.distanceY, _this.distanceY);
 
-          _this.move.moveX = range(moveX, -distanceX, distanceX);
-          _this.move.moveY = range(moveY, -distanceY, distanceY);
+          _this.imgageSacaleAnimate(false);
 
-          _this.imageScale();
-        } // event.preventDefault();
+          _this.imageScale(); // event.preventDefault();
 
+        }
+
+        event.preventDefault();
       };
 
       this.documnetTouchEnd = function (event) {
+        console.log('触摸结束');
+
+        if (_this.store.scale < 1) {
+          _this.store.scale = 1;
+        }
+
         if (_this.store.scale === 1) {
           _this.swiper.enable();
         } else {
           _this.swiper.disable();
         }
 
+        if (!event.touches.length) {
+          // event.preventDefault();
+          _this.distanceX = _this.maxMoveX();
+          _this.distanceY = _this.maxMoveY();
+          _this.move.moveX = range(_this.move.moveX, -_this.distanceX, _this.distanceX);
+          _this.move.moveY = range(_this.move.moveY, -_this.distanceY, _this.distanceY);
+
+          _this.imgageSacaleAnimate(true);
+
+          _this.imageScale();
+        }
+
         _this.move.moveing = false;
-        _this.move.moveable = false;
+        _this.move.moveable = false; // this.move.moveX = range(this.move.moveX, -this.distanceX, this.distanceX);
+        // this.move.moveY = range(this.move.moveX, -this.distanceY, this.distanceY);
+        // this.imgageSacaleAnimate(true)
+        // this.imageScale();
       };
 
       this.documentDblClick = function (event) {
@@ -363,12 +421,26 @@
         }, 500);
 
         if (_this._dblclick > 1) {
-          _this.store.scale = _this.store.scale === 1 ? 3 : 1;
+          _this.store.scale = _this.store.scale === MAXSCALENUM ? 1 : MAXSCALENUM;
           _this.move.moveX = 0;
           _this.move.moveY = 0;
 
+          _this.imgageSacaleAnimate(true);
+
           _this.imageScale();
+
+          if (_this.store.scale === 1) {
+            _this.swiper.enable();
+          } else {
+            _this.swiper.disable();
+          }
         }
+
+        setTimeout(function () {
+          if (_this._dblclick === 1) {
+            _this.open && _this.close();
+          }
+        }, 300);
       };
 
       document.addEventListener('touchstart', this.documnetTouchStart, {
@@ -380,50 +452,65 @@
       document.addEventListener('touchend', this.documnetTouchEnd, {
         passive: false
       });
-      document.addEventListener('click', this.documentDblClick); // const { index, swiperLis } = this.swiper;
-      // const image = swiperLis[index].querySelector('img')
-      // image.onclick = function() {
-      //     alert(1)
-      // }
+      this.swiper.container.addEventListener('click', this.documentDblClick);
+    },
+    close: function close() {
+      this.open = false;
+      document.removeEventListener('touchstart', this.documnetTouchStart);
+      document.removeEventListener('touchmove', this.documnetTouchMove);
+      document.removeEventListener('touchend', this.touchend);
+      this.swiper.container.removeEventListener('click', this.documentDblClick);
+      this.swiper.destrory();
     },
     imageScale: function imageScale() {
       var _this$swiper = this.swiper,
           index = _this$swiper.index,
           swiperLis = _this$swiper.swiperLis;
-      var scale = this.store.scale;
+      var scale = this.store.scale; // if (scale !== 1) { 
 
-      if (scale !== 1) {
-        var offsetX = this.move.moveX / scale;
-        var offsetY = this.move.moveY / scale;
-        console.log(offsetY);
-        var image = swiperLis[index].querySelector('.swiper-wrap-preview__image');
-        image.style.transform = "scale(".concat(scale, ", ").concat(scale, ") translate(").concat(offsetX, "px, ").concat(offsetY, "px)");
-      } // alert(window.innerWidth)
-
+      var offsetX = this.move.moveX / scale;
+      var offsetY = this.move.moveY / scale;
+      var image = swiperLis[index].querySelector('.swiper-wrap-preview__image');
+      image.style.transform = "scale(".concat(scale, ", ").concat(scale, ") translate(").concat(offsetX, "px, ").concat(offsetY, "px)"); // }
+      // alert(window.innerWidth)
     },
-    maxMoveX: function maxMoveX() {
-      var maxMoveX = 0;
+    imgageSacaleAnimate: function imgageSacaleAnimate(type) {
       var _this$swiper2 = this.swiper,
           index = _this$swiper2.index,
           swiperLis = _this$swiper2.swiperLis;
       var image = swiperLis[index].querySelector('.swiper-wrap-preview__image');
-      var width = image.offsetWidth;
 
-      if (width * this.store.scale > window.innerWidth) {
-        maxMoveX = (width * this.store.scale - window.innerWidth) / 2;
-        maxMoveX = Math.max(0, maxMoveX);
+      if (type) {
+        image.style['transitionDuration'] = '.3s';
+      } else {
+        image.style['transitionDuration'] = '0s'; // console.log(image.style['transitionDuration'])
       }
-
-      return maxMoveX;
     },
-    maxMoveY: function maxMoveY() {
-      var maxMoveY = 0;
+    maxMoveX: function maxMoveX() {
+      var maxMoveX = 0;
       var _this$swiper3 = this.swiper,
           index = _this$swiper3.index,
           swiperLis = _this$swiper3.swiperLis;
       var image = swiperLis[index].querySelector('.swiper-wrap-preview__image');
-      var height = image.offsetHeight;
-      console.log(height * this.store.scale > window.innerHeight);
+      var width = image.offsetWidth;
+
+      if (image.offsetHeight === window.innerHeight) {
+        maxMoveX = 0;
+      } else if (width * this.store.scale > window.innerWidth) {
+        maxMoveX = (width * this.store.scale - window.innerWidth) / 2;
+        maxMoveX = Math.max(0, maxMoveX);
+      }
+
+      console.log(maxMoveX);
+      return maxMoveX;
+    },
+    maxMoveY: function maxMoveY() {
+      var maxMoveY = 0;
+      var _this$swiper4 = this.swiper,
+          index = _this$swiper4.index,
+          swiperLis = _this$swiper4.swiperLis;
+      var image = swiperLis[index].querySelector('.swiper-wrap-preview__image');
+      var height = image.offsetHeight; // console.log(height * this.store.scale > window.innerHeight)
 
       if (height * this.store.scale > window.innerHeight) {
         maxMoveY = (height * this.store.scale - window.innerHeight) / 2;
@@ -439,8 +526,9 @@
   function createSwiper(options) {
     var html = createHtml();
     document.body.appendChild(html);
-    var slider = new Touches(imagePreveiwId, options);
-    new scaleImage(slider);
+    var sliderInstence = new Touches(imagePreveiwId, options);
+    var scaleImageInstence = new scaleImage(sliderInstence);
+    return scaleImageInstence;
   }
 
   function createHtml(options) {
@@ -456,13 +544,20 @@
 
   function imagePreview() {}
 
+  imagePreview.instance = {};
+
   imagePreview.open = function (options) {
     options.index = options.index ? options.index : 0;
     options.list = options.list ? options.list : [];
-    createSwiper(options);
+    imagePreview.instance = createSwiper(options);
+    setTimeout(function () {
+      imagePreview.instance.open = true;
+    }, 500);
   };
 
-  imagePreview.close = function (options) {};
+  imagePreview.close = function (options) {
+    imagePreview.instance.close();
+  };
 
   // import './css/index.css'
 
